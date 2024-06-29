74c741a89863996613bce507e5dbce6991ac50ec
ravitejaemandi <teju.emandi@gmail.com>
2024-06-29 11:53:43 +0530

    - Initial Commit

---
+++ src/main/java/com/raviteja/ravitejalibrary/Utils.java
@@ -0,0 +1,10 @@
+package com.raviteja.ravitejalibrary;
+
+/**
+ * Created by Raviteja Emandi on 29,June,2024
+ **/
+public class Utils {
+    public static String sampleMethod() {
+        return "Hai Sir, How are you?";
+    }
+}
